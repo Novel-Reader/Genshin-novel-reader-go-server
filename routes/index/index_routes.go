75c2adb70a1c32afc00ad28663f48500b31a8d4a
Michael An <2331806369@qq.com>
2025-03-09 11:40:02 +0800

    06 use html template

--- routes/index/index_routes.go
+++ routes/index/index_routes.go
@@ -3,10 +3,23 @@ package index
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"text/template"
 )
 
 func IndexRoutes(r *gin.RouterGroup) {
 	r.GET("/", func(c *gin.Context) {
-		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte("<html><body>Welcome to use Genshin novel reader go server</body></html>"))
+		t, err := template.New("welcome").ParseFiles("templates/welcome.html")
+		if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+		data := struct {
+			Title string
+			Body  string
+		}{
+			Title: "Genshin novel reader go server",
+			Body:  "Welcome to use Genshin novel reader go server",
+		}
+		t.ExecuteTemplate(c.Writer, "welcome.html", data)
 	})
 }
